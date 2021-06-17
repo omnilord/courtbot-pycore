25b3f6756d4f2f88f6501c44c405325854c69c23
Eric L. Truitte, MBA <omnilord@privateerconsulting.com>
2021-06-16 20:08:44 -0400

    fix: correct minimum python version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     #install_requires=requirements,
     packages=find_packages(),
     license='LICENSE.txt',
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     scripts=[],
     url='http://pypi.python.org/pypi/courtbot-pycore/',
 )
